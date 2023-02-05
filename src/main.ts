eb8258dfba0e93d016386e06fc2c9c005705441f
Stanislav Kolpakov <kolpakoff@gmail.com>
2023-02-05 21:24:14 +0200

    feat: Attach swagger to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = parse(
+    (await readFile(join(process.cwd(), 'doc', 'api.yaml'))).toString('utf-8'),
+  );
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
